bb7b0bab38be0ea7b85f38235842aebcef807fda
Nathaniel <natharevolution@gmail.com>
2023-01-05 10:54:29 -0400

    agregando funcion de subir imagenes con su respectivo servicio

--- escuela/src/app/components/instripciones/instripciones.component.ts
+++ escuela/src/app/components/instripciones/instripciones.component.ts
@@ -3,23 +3,27 @@ import { Component, OnInit } from '@angular/core';
 // importrar modelo (de estudiantes) y servicio(de estudiantes).
 import { Estudiantes } from 'src/app/models/Estudiantes';
 import { EstudiantesService } from 'src/app/services/estudiantes.service';
+import { UploadService } from 'src/app/services/upload.service';
+import { global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-instripciones',
   templateUrl: './instripciones.component.html',
   styleUrls: ['./instripciones.component.css'],
   // decorador con el servicio en la propiedad providers.
-  providers: [EstudiantesService],
+  providers: [EstudiantesService, UploadService],
 })
 export class InstripcionesComponent implements OnInit {
   public title: string;
   public estudiantes: Estudiantes;
-  public status: String
+  public status: string
+  public FilesToUpload: Array<File> 
 
-  constructor(private _estudiantesService: EstudiantesService) {
+  constructor(private _estudiantesService: EstudiantesService, private _uploadImage: UploadService) {
     this.status = "";
     this.title = 'Inscripciones de alumnos';
     this.estudiantes = new Estudiantes('', '', '', 0, '', '', '');
+    this.FilesToUpload = [];
   }
 
   ngOnInit(): void {}
@@ -29,10 +33,19 @@ export class InstripcionesComponent implements OnInit {
     /* se instancia el servicio con la funcion con su respectivo parametro y 
     con el metodo subscribe que recibe un la response y el error
     */
+    //  subida de infomacion.
     this._estudiantesService.guardar_alumno(this.estudiantes).subscribe(
       (response) => {
         if (response.estudiantes) {
-          this.status = "success"
+          
+          // subida de imagenes.
+          this._uploadImage.uploadImage(global.url + "subir-image" +response.estudiantes._id, [], this.FilesToUpload, 'image').then((result: any) => {
+            this.status = "success"
+            console.log(result);
+            
+          });
+          form.reset();
+          this
         } else {
           this.status = "failed";
         }
@@ -41,5 +54,9 @@ export class InstripcionesComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  fileUpload(fileInput:any){
+    this.FilesToUpload = <Array<File>>fileInput.target.files;
   }
 }
