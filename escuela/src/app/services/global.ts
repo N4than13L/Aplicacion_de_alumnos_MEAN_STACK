a339be48dc527f6d3ae7afa0b8d2bd354740be8c
Nathaniel <natharevolution@gmail.com>
2023-03-14 23:07:29 -0400

    agregando enlace del api al front-end

--- escuela/src/app/services/global.ts
+++ escuela/src/app/services/global.ts
@@ -1,5 +1,5 @@
 export var global = {
-  url: 'http://localhost:3377/api/',
+  url: 'https://aplicacion-de-alumnos-mean-stack-api.vercel.app/api/',
   url_maestros: "https://reqres.in/api/users?page=1",
   url_maestros_tecnicos: "https://reqres.in/api/users?page=2"
 };
