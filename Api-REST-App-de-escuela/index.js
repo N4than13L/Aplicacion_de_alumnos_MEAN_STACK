25151d1f21570a30e1d360a82ce690c3c8e79fbc
Nathaniel <natharevolution@gmail.com>
2023-03-14 22:47:25 -0400

    actualizando el framework y agregando string de base de datos eremota seguido de correguir error de UI

--- Api-REST-App-de-escuela/index.js
+++ Api-REST-App-de-escuela/index.js
@@ -11,8 +11,11 @@ var port = 3377;
 // conexion en remoto.
 // mongodb+srv://natharevolution:jcEuOEQjFZ74jI8t@escuela-mean-stack.nrmfvpw.mongodb.net/?retryWrites=true&w=majority
 // contrasena: jcEuOEQjFZ74jI8t
+
 mongoose
-  .connect("mongodb://localhost:27017/estudiantes")
+  .connect(
+    "mongodb+srv://natharevolution:jcEuOEQjFZ74jI8t@escuela-mean-stack.nrmfvpw.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     app.listen(port, () => {
       console.log(
